bc66b04f3ed7784a9e88bb798f90338296d83430
Tobias Kuhn <kuhntobias@gmail.com>
2025-04-10 11:23:54 +0200

    Allow POST request at / or /np/

--- src/main/java/com/knowledgepixels/registry/MainVerticle.java
+++ src/main/java/com/knowledgepixels/registry/MainVerticle.java
@@ -18,10 +18,12 @@ import org.nanopub.extra.server.PublishNanopub;
 import com.mongodb.client.ClientSession;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import net.trustyuri.TrustyUriUtils;
 
 public class MainVerticle extends AbstractVerticle {
@@ -59,52 +61,49 @@ public class MainVerticle extends AbstractVerticle {
 			MainPage.show(c);
 		});
 
-		router.route(HttpMethod.POST, "/").handler(c -> {
-			if (c.request().path().equals("/")) {
-				c.request().bodyHandler(bh -> {
+		Handler<RoutingContext> postHandler = c -> {
+			c.request().bodyHandler(bh -> {
+				try {
+					String contentType = c.request().getHeader("Content-Type");
+					Nanopub np = null;
 					try {
-						String contentType = c.request().getHeader("Content-Type");
-						Nanopub np = null;
-						try {
-							np = new NanopubImpl(bh.toString(), Rio.getParserFormatForMIMEType(contentType).orElse(RDFFormat.TRIG));
-						} catch (MalformedNanopubException ex) {
-							ex.printStackTrace();
-						}
-						if (np != null) {
-							try (ClientSession s = RegistryDB.getClient().startSession()) {
-								String ac = TrustyUriUtils.getArtifactCode(np.getUri().toString());
-								if (has(s, "nanopubs", ac)) {
-									System.err.println("POST: known nanopub " + ac);
-								} else {
-									System.err.println("POST: new nanopub " + ac);
-	
-									// TODO Run checks here whether we want to register this nanopub (considering quotas etc.)
-									NanopubLoader.simpleLoad(s, np);
+						np = new NanopubImpl(bh.toString(), Rio.getParserFormatForMIMEType(contentType).orElse(RDFFormat.TRIG));
+					} catch (MalformedNanopubException ex) {
+						ex.printStackTrace();
+					}
+					if (np != null) {
+						try (ClientSession s = RegistryDB.getClient().startSession()) {
+							String ac = TrustyUriUtils.getArtifactCode(np.getUri().toString());
+							if (has(s, "nanopubs", ac)) {
+								System.err.println("POST: known nanopub " + ac);
+							} else {
+								System.err.println("POST: new nanopub " + ac);
 
-									if (!isSet(s, "serverInfo", "testInstance")) {
-										// Here we publish it also to the first-generation services, so they know about it too:
-										// TODO Remove this at some point
-										try {
-											PublishNanopub.publish(np);
-										} catch (IOException ex) {
-											ex.printStackTrace();
-										}
+								// TODO Run checks here whether we want to register this nanopub (considering quotas etc.)
+								NanopubLoader.simpleLoad(s, np);
+
+								if (!isSet(s, "serverInfo", "testInstance")) {
+									// Here we publish it also to the first-generation services, so they know about it too:
+									// TODO Remove this at some point
+									try {
+										PublishNanopub.publish(np);
+									} catch (IOException ex) {
+										ex.printStackTrace();
 									}
 								}
 							}
 						}
-						c.response().setStatusCode(201);
-					} catch (Exception ex) {
-						c.response().setStatusCode(400).setStatusMessage("Error processing nanopub: " + ex.getMessage());
-					} finally {
-						c.response().end();
 					}
-				});
-					
-			} else {
-				c.response().setStatusCode(400).setStatusMessage("Invalid POST request: " + c.request().path());
-			}
-		});
+					c.response().setStatusCode(201);
+				} catch (Exception ex) {
+					c.response().setStatusCode(400).setStatusMessage("Error processing nanopub: " + ex.getMessage());
+				} finally {
+					c.response().end();
+				}
+			});
+		};
+		router.route(HttpMethod.POST, "/").handler(postHandler);
+		router.route(HttpMethod.POST, "/np/").handler(postHandler);
 
 		// INIT
 		vertx.executeBlocking(() -> {
