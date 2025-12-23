533322e3d87af76297339ad9a92a75bc5215a316
Ashley Caselli <ashley@knowledgepixels.com>
2025-12-23 15:41:17 +0100

    chore(collection): add Collection enum for registry constants

---
+++ src/main/java/com/knowledgepixels/registry/Collection.java
@@ -0,0 +1,22 @@
+package com.knowledgepixels.registry;
+
+public enum Collection {
+
+    SERVER_INFO("serverInfo"),
+    SETTING("setting"),
+    AGENTS("agents"),
+    ACCOUNTS("accounts"),
+    NANOPUBS("nanopubs");
+
+    private final String value;
+
+    Collection(final String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+}
