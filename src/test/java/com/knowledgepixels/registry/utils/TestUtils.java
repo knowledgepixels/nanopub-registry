271be82f0a3a96bf9119f704f3adae4e31ca0ed6
Ashley Caselli <ashley@knowledgepixels.com>
2026-01-22 15:38:03 +0100

    chore(TestUtils): add utility class for test environment setup and static field clearing

---
+++ src/test/java/com/knowledgepixels/registry/utils/TestUtils.java
@@ -0,0 +1,45 @@
+package com.knowledgepixels.registry.utils;
+
+import com.knowledgepixels.registry.ReadsEnvironment;
+import com.knowledgepixels.registry.Utils;
+import org.testcontainers.mongodb.MongoDBContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Utility class for setting up test environments and clearing static fields.
+ */
+public class TestUtils {
+
+    /**
+     * Sets up a fake environment for testing with the provided MongoDB container.
+     *
+     * @param mongoDBContainer the MongoDB container to use for the fake environment
+     */
+    public static void setupFakeEnv(MongoDBContainer mongoDBContainer) {
+        Map<String, String> fakeEnv = new HashMap<>();
+        fakeEnv.put("REGISTRY_DB_NAME", "nanopubRegistry");
+        fakeEnv.put("REGISTRY_DB_HOST", mongoDBContainer.getHost());
+        fakeEnv.put("REGISTRY_DB_PORT", String.valueOf(mongoDBContainer.getFirstMappedPort()));
+        ReadsEnvironment reader = new ReadsEnvironment(fakeEnv::get);
+        Utils.setEnvReader(reader);
+    }
+
+    /**
+     * Clears the specified static fields of the given class by setting them to null.
+     *
+     * @param clazz      the class whose static fields are to be cleared
+     * @param fieldNames the names of the static fields to be cleared
+     * @throws IllegalAccessException thrown if the field cannot be accessed
+     * @throws NoSuchFieldException   thrown if the field does not exist
+     */
+    public static void clearStaticFields(Class<?> clazz, String... fieldNames) throws IllegalAccessException, NoSuchFieldException {
+        for (String fieldName : fieldNames) {
+            var field = clazz.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(null, null);
+        }
+    }
+
+}
